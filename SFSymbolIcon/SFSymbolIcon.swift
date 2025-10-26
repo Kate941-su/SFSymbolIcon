576fd6c43928f7f3835617e40a1ef966099faa69
Kaito Kitaya <kaito.kitaya.deutschland@gmail.com>
2025-10-26 09:48:21 +0100

    Initial Commit

---
+++ SFSymbolIcon/SFSymbolIcon.swift
@@ -0,0 +1,9 @@
+//
+//  SFSymbolIcon.swift
+//  SFSymbolIcon
+//
+//  Created by Kaito Kitaya on 26.10.25.
+//
+
+import Foundation
+
---
+++ SFSymbolIconTests/SFSymbolIconTests.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolIconTests.swift
+//  SFSymbolIconTests
+//
+//  Created by Kaito Kitaya on 26.10.25.
+//
+
+import Testing
+@testable import SFSymbolIcon
+
+struct SFSymbolIconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
